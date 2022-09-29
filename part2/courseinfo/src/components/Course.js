42d7c59ab068939f8a282cca51652fb649131c01
Eric Tsang <etsang647@gmail.com>
2022-09-29 19:09:30 -0400

    finished part 2a 2.1-2.5

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,43 @@
+const Header = ({ name }) => {
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>{name} {exercises}</p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((prev, cur) => prev + cur.exercises, 0)
+  return (
+    <b>
+      total of {total} exercises
+    </b>
+  )
+}
+
+const Course = ({ course }) => {
+  const { name, parts } = course
+  return (
+    <div>
+      <Header name={name} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default Course
